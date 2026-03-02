12f4dc4544225878408c419944effbc7c500da3d
eys29 <eys29@cornell.edu>
2026-03-02 14:46:18 -0600

    print full kv cache

--- mymodel.py
+++ mymodel.py
@@ -3,9 +3,12 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, DynamicCache
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", dtype=torch.float16, device_map="auto")
-inputs = tokenizer("I like rock music because", return_tensors="pt").to(model.device)
+inputs = tokenizer("My sister loves jazz music but I like rock music because", return_tensors="pt").to(model.device)
 
 past_key_values = DynamicCache(config=model.config)
 outputs = model(**inputs, past_key_values=past_key_values, use_cache=True)
+torch.set_printoptions(profile="full")
+print("keys shape: " + str(outputs.past_key_values.layers[0].keys.shape)) 
+print("values shape: " + str(outputs.past_key_values.layers[0].values.shape)) 
 print(outputs.past_key_values.layers[0].keys) 
 print(outputs.past_key_values.layers[0].values) 
