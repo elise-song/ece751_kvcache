4297f30b89a0da954bf31f745076e369e2331523
Elise Song <elise.song@wisc.edu>
2026-05-03 22:47:40 -0500

    pull longbench

---
+++ scripts/LongBench/pull_longbench.py
@@ -0,0 +1,6 @@
+from datasets import load_dataset
+
+datasets = ["2wikimqa", "gov_report", "multi_news", "repobench-p"]
+
+for dataset in datasets:
+    data = load_dataset('THUDM/LongBench', dataset, split='test')
